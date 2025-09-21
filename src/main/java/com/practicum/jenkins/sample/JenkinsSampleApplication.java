bf8013bddecd58d6b250e0e2ebaf8c0b86f6fc5b
viktar-b <drmhere@yandex.by>
2025-09-21 18:45:57 +0300

    #app name fix

--- src/main/java/com/practicum/jenkins/sample/JenkinsSampleApplication.java
+++ src/main/java/com/practicum/jenkins/sample/JenkinsSampleApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JenkinsExampleApplication {
+public class JenkinsSampleApplication {
     public static void main(String[] args) {
-        SpringApplication.run(JenkinsExampleApplication.class, args);
+        SpringApplication.run(JenkinsSampleApplication.class, args);
     }
 }
